fe03516cfb9841c0cad89673e3357e837565bf30
egonzal03 <86126468+egonzal03@users.noreply.github.com>
2021-07-15 18:35:13 -0400

    Update Game.java
    added some fields and method functionality to Game class

--- serving-web-content/src/main/java/com/example/Game/Game.java
+++ serving-web-content/src/main/java/com/example/Game/Game.java
@@ -3,19 +3,30 @@ package com.example.Game;
 import java.util.ArrayList;
 import com.example.Player.Player;
 
+/**
+ * This class represents the game state for all players.
+ */
 public class Game{
     /* Instantiate Fields Here*/
-    ArrayList<Player> playerList = ArrayList<Player>();
-
+    private ArrayList<Player> playerList ;
+    private int playerListIndex;
+    private Player currentActivePlayer;
 
     
     /* Constructor */
     public Game(){
-
+        this.playerList = new ArrayList<Player>();
+        this.playerListIndex = 0;
     }
 
+    /**
+     * assigns a player as the curent active player
+     */
     public void assignActivePlayer(){
-
+        // the player list index will continue to increment
+        // the modulus ensures the index will remain within the bounds of the array
+        currentActivePlayer = playerList.get(playerListIndex % playerList.size());
+        ++playerListIndex;
     }
 
     public void createTurnForPlayer(){
