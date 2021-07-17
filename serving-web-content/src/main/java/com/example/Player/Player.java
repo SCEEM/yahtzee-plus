737633f298ea85b56e6dba1ca5fb324b1f8bf11d
Simran <sshinh1225@gmail.com>
2021-07-17 01:00:29 -0400

    Updated constructor for Player

--- serving-web-content/src/main/java/com/example/Player/Player.java
+++ serving-web-content/src/main/java/com/example/Player/Player.java
@@ -4,15 +4,24 @@ import com.example.Turn.Turn;
 
 public class Player {
     /* Instantiate Fields Here*/
-    Turn currentTurn;
+    private Turn currentTurn;
+    private boolean isHost; 
+    private int playerId; 
+    private String playerName; 
+    private int scoreCardId; 
 
     /* Constructor */
     public Player(){
-
+        this.currentTurn = false; 
+        this.playerId = playerId;
+        this.playerName = playerName; 
+        this.isHost = isHost; 
+        this.scoreCardId = scoreCardId;
     }
 
     public void takeTurn(){
-
+        currentTurn = true; 
+        
     }
 
     /* May be moved to inside the takeTurn() method */
