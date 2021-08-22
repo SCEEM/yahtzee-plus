05a2270c1727a05a27a59769d5c253622cff05b7
egonzal03 <86126468+egonzal03@users.noreply.github.com>
2021-08-21 21:24:06 -0400

    update to chat - added welcome page
    added system messaging functionality to app.js
    
    added a welcome page that will show the main content page after submitting a name.
    
    game class now has a check to ensure that the game is not played beyond 13 rounds

--- serving-web-content/src/main/java/com/yahtzee/game/Game.java
+++ serving-web-content/src/main/java/com/yahtzee/game/Game.java
@@ -12,9 +12,9 @@ public class Game {
   private ArrayList<Player> playerList;
   private ArrayList<Integer> scoreList;
   private int playerListIndex;
-  private Player currentActivePlayer, system;
+  private Player currentActivePlayer;
   private static final int MAX_PLAYERS = 6;
-  // private Chat chat;
+  private final int MAX_ROUNDS = 13;
 
 
   /**
@@ -24,8 +24,6 @@ public class Game {
     this.playerList = new ArrayList<>();
     this.playerListIndex = 0;
     this.currentActivePlayer = null; // placeholder for assignActivePlayer functionality
-    this.system = new Player(-1); // a representation of the system for Chat purposes
-    // this.chat = new Chat();
     this.scoreList = new ArrayList<Integer>();
   }
 
@@ -73,11 +71,22 @@ public class Game {
    * Assigns a player as the current active player
    */
   public void assignNextActivePlayer() {
-    // the player list index will continue to increment
+  // the player list index will continue to increment
     // the modulus ensures the index will remain within the bounds of the array
     ++playerListIndex;
-    currentActivePlayer = playerList.get(playerListIndex % playerList.size());
-    currentActivePlayer.startTurn();
+    if (gameHasRoundsLeft()){ // will not assign another player to be active if there are no more rounds to play
+      currentActivePlayer = playerList.get(playerListIndex % playerList.size());
+      currentActivePlayer.startTurn();
+    }
+  }
+
+  /**
+   * will check if the game has exceeded 13 rounds of play
+   * @return True if the game is on it 13th or lesser round, false otherwise
+   */
+  private boolean gameHasRoundsLeft(){
+    int roundIdx = playerListIndex / playerList.size(); // returns a new, incremented whole number when the game returns back to the first player
+    return roundIdx < MAX_ROUNDS;
   }
 
   
